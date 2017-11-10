06c8da31e404a43e7bfc1b14478148d35a5a7492
JohannBillar <jwbillar@gmail.com>
2017-11-09 16:04:21 -0800

    Add details view

--- src/js/components/DoWhop.jsx
+++ src/js/components/DoWhop.jsx
@@ -2,15 +2,24 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
+import { connect } from 'react-redux';
 import DoWhopButton from './DoWhopButton';
 
-const DoWhop = () => (
-  <div>
-    <h1>I am a DoWhop Detail View</h1>
-    <Link to="/dashboard/">
-      <DoWhopButton>Back</DoWhopButton>
-    </Link>
-  </div>
-);
+const DoWhop = (props: { match: Match }) => {
+  const { match } = props;
+  const dowhopDescriptionKey = match.params.dowhopDescriptionKey;
+  return (
+    <div>
+      <h1>I am a DoWhop Detail View</h1>
+      <p>{dowhopDescriptionKey}</p>
+      <Link to="/dashboard/">
+        <DoWhopButton>Back</DoWhopButton>
+      </Link>
+    </div>
+  );
+};
 
-export default DoWhop;
+const mapStateToProps = ({ doWhopDescriptions }) => ({ doWhopDescriptions });
+
+export default connect(mapStateToProps)(DoWhop);
