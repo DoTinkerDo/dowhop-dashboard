de6b67424c142aa5c19e57d660d9288afe21d13d
JohannBillar <jwbillar@gmail.com>
2017-11-15 14:50:38 -0800

    Change DoWhopDescription order to most recent at the top

--- src/js/components/DoWhopDescriptionsIndex.jsx
+++ src/js/components/DoWhopDescriptionsIndex.jsx
@@ -5,18 +5,22 @@ import { map } from 'lodash';
 import { Row, Col } from 'reactstrap';
 import DoWhopDescriptionCard from './DoWhopDescriptionCard';
 
-const DoWhopDescriptionsIndex = ({ doWhopDescriptions }: Object) => (
-  <div>
-    <Row>
-      <Col>
-        <div className="dowhop-grid">
-          {map(doWhopDescriptions, doWhopDescription => (
-            <DoWhopDescriptionCard key={doWhopDescription.doWhopDescriptionKey} {...doWhopDescription} />
-          ))}
-        </div>
-      </Col>
-    </Row>
-  </div>
-);
+const DoWhopDescriptionsIndex = ({ doWhopDescriptions }: Object) => {
+  const arrayDescription = map(doWhopDescriptions);
+  const descendingDescriptions = arrayDescription.reverse();
+  return (
+    <div>
+      <Row>
+        <Col>
+          <div className="dowhop-grid">
+            {map(descendingDescriptions, doWhopDescription => (
+              <DoWhopDescriptionCard key={doWhopDescription.doWhopDescriptionKey} {...doWhopDescription} />
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default DoWhopDescriptionsIndex;
